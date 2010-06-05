1c95b121dfa2d9001a8a91a55a964c5e23d7cda5
Otto Allmendinger <otto.allmendinger@googlemail.com>
2010-06-06 01:45:56 +0200

    fixed build (path for numpy/arrayobject.h)

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy
+
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("cells_helpers", ["cells_helpers.pyx"])]
+    ext_modules = [Extension("cells_helpers", ["cells_helpers.pyx"],
+        include_dirs = [numpy.get_include(), '.']
+    )]
 )
